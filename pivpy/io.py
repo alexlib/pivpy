b978799745a56fab1022d5e08449ffd707e4e4f7
Alex Liberzon <alex.liberzon@gmail.com>
2018-06-11 01:37:54 +0300

    io.py now has xarray
    works with Python 3.6 only

--- pivpy/io.py
+++ pivpy/io.py
@@ -5,6 +5,7 @@ Contains functions for reading flow fields in various formats
 """
 
 import numpy as np
+import xarray as xr
 from glob import glob
 import os.path
 
@@ -40,12 +41,26 @@ def parse_header(filename):
 
 def loadvec(filename,rows=None, cols=None):
     """
-        loadvec(filename)
+        loadvec(filename,rows=rows,cols=cols)
+        Loads the VEC file (TECPLOT format by TSI Inc.) and OpenPIV format
+        Arguments:
+            filename : file name, expected to have a header and 5 columns
+            rows, cols : number of rows and columns of a vector field,
+            if None, None, then parse_header is called to infer the number
+            written in the header
+        Output:
+            data is a xAarray Dataset, see xarray for help 
     """
-    d = np.loadtxt(filename,skiprows=1,delimiter=',',
-        dtype={'names': ('x', 'y', 'u', 'v'),'formats': ('f4', 'f4', 'f4', 'f4')},
-        usecols=(0,1,2,3)).reshape(rows,cols)
-    return d
+    if rows is None or cols is None:
+        _,_,rows,cols = parse_header(filename)
+    d = np.loadtxt(filename,skiprows=1,delimiter=',',usecols=(0,1,2,3,4)).reshape(rows,cols,5)
+    
+    u = xr.DataArray(d[:,:,2],dims=('x','y'),coords={'x':d[:,:,0][0,:],'y':d[:,:,1][:,0]})
+    v = xr.DataArray(d[:,:,3],dims=('x','y'),coords={'x':d[:,:,0][0,:],'y':d[:,:,1][:,0]})
+    cnc = xr.DataArray(d[:,:,4],dims=('x','y'),coords={'x':d[:,:,0][0,:],'y':d[:,:,1][:,0]})
+    data = xr.Dataset({'u': u, 'v': v,'cnc':cnc})  
+    
+    return data
     
     
     

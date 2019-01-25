aeb8eadcc15fb14eea3095f0e9889a1647c1ef53
Alex Liberzon <alex.liberzon@gmail.com>
2019-01-26 01:04:49 +0200

    so now we use xarray concatenate efficiently, plot using attributes as labels

--- pivpy/graphics.py
+++ pivpy/graphics.py
@@ -34,27 +34,40 @@ def showf_ownclass(data, var=None, units=None, fig=None):
     pl.show()
         
          
-def showf(data, var=None, units=None, fig=None):
+def showf(data, variables=None, units=None, fig=None):
     """ 
     showf(data, var, units)
     Arguments:
         data : xarray.DataSet that contains dimensions of t,x,y
                and variables u,v and maybe w (scalar)
     """
-    fig = pl.figure(None if fig is None else fig.number)
-    # import pdb; pdb.set_trace()
-    # xlabel = (None if var is None else var[0]) + ' [' + (None if units is None else units[0])+']'
-    # ylabel = (None if var is None else var[1]) + ' [' + (None if units is None else units[1])+']'
-        
-    
-    for t in data['t']:
-        d = data.isel(t=t)
-        pl.quiver(d['x'],d['y'],d['u'],d['v'],d['u']**2 + d['v']**2)
+
+    if variables is None:
+        xlabel = ' '
+        ylabel = ' '
+    else:
+        xlabel = variables[0]
+        ylabel = variables[1]
+
+    if units is not None:
+        xlabel += ' ' + units[0]
+        ylabel += ' ' + units[1]
+
+
+    fig = pl.figure(None if fig is None else fig.number)  
+    if 't' in data.dims:
+        for t in data['t']:
+            d = data.isel(t=t)
+            pl.quiver(d['x'],d['y'],d['u'],d['v'],d['u']**2 + d['v']**2)
+            pl.xlabel(xlabel)
+            pl.ylabel(ylabel)
+            pl.draw()
+            pl.pause(0.1)
+    else:
+        pl.quiver(data['x'],data['y'],data['u'],data['v'],data['u']**2 + data['v']**2)
         pl.xlabel(xlabel)
         pl.ylabel(ylabel)
-        pl.title(str(k))
         pl.draw()
-        pl.pause(0.1)
         
     pl.show()  
 
--- pivpy/io.py
+++ pivpy/io.py
@@ -10,20 +10,51 @@ from glob import glob
 import os.path
 
 
-def loadvec_dir(directory):
-    """ loadvec_dir (directory)
+def load_directory(directory):
+    """ 
+    load_directory (path)
+
+    Loads all the .VEC files in the directory into a single
+    xarray dataset with variables and units added as attributes
+
+    Input: 
+        directory : path to the directory with .vec files
+
+    Output:
+        data : xarray DataSet with dimensions: x,y,t and 
+               data arrays of u,v,
+               attributes of variables and units
+
+
+    See more: loadvec
     """
     files  = glob(os.path.join(directory,'*.vec'))
     variables, units, rows, cols = parse_header(files[0])
     
     data = []
     for f in files:
-        data.append(loadvec(f,rows,cols))
+        data.append(loadvec(f,rows,cols,variables,units))
+           
     
-    return (data, variables, units) 
+    combined = xr.concat(data, dim='t')
+    combined.attrs['variables'] = variables
+    combined.attrs['units'] = units
+    return combined
 
 def parse_header(filename):
-    """ parse_header (filename) """
+    """ 
+    parse_header ( filename )
+    Parses header of the file (.vec) to get the variables (typically X,Y,U,V)
+    and units (can be m,mm, pix/dt or mm/sec, etc.), and the size of the dataset
+    by the number of rows and columns.
+    Input:
+        filename : complete path of the file to read
+    Returns:
+        variables : list of strings
+        units : list of strings
+        rows : number of rows of the dataset
+        cols : number of columns of the dataset 
+    """
     with open(filename) as fid:
         header = fid.readline()
 
@@ -39,7 +70,7 @@ def parse_header(filename):
     
         
 
-def loadvec(filename,rows=None, cols=None):
+def loadvec(filename, rows=None, cols=None, variables=None, units=None):
     """
         loadvec(filename,rows=rows,cols=cols)
         Loads the VEC file (TECPLOT format by TSI Inc.) and OpenPIV format
@@ -52,13 +83,19 @@ def loadvec(filename,rows=None, cols=None):
             data is a xAarray Dataset, see xarray for help 
     """
     if rows is None or cols is None:
-        _,_,rows,cols = parse_header(filename)
+        variables,units,rows,cols = parse_header(filename)
+
     d = np.loadtxt(filename,skiprows=1,delimiter=',',usecols=(0,1,2,3,4)).reshape(rows,cols,5)
     
     u = xr.DataArray(d[:,:,2],dims=('x','y'),coords={'x':d[:,:,0][0,:],'y':d[:,:,1][:,0]})
     v = xr.DataArray(d[:,:,3],dims=('x','y'),coords={'x':d[:,:,0][0,:],'y':d[:,:,1][:,0]})
     cnc = xr.DataArray(d[:,:,4],dims=('x','y'),coords={'x':d[:,:,0][0,:],'y':d[:,:,1][:,0]})
-    data = xr.Dataset({'u': u, 'v': v,'cnc':cnc})  
+    data = xr.Dataset({'u': u, 'v': v,'cnc':cnc})
+
+
+    if variables is not None:
+        data.attrs['variables'] = variables
+        data.attrs['units'] = units  
     
     return data
     

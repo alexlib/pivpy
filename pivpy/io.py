fixed graphics - quiver now differentiates the data with units (shows in units directions) or pixels - then the top left corner is 0,0
bumps to 0.0.07

pivpy/io.py
@@ -21,7 +21,7 @@ def create_sample_field(rows=5,cols=8,frame = 0):
 
     xm,ym = np.meshgrid(x,y)
 
-    u = np.ones_like(xm.T) + np.linspace(0.0,7.0,rows)
+    u = np.ones_like(xm.T) + np.linspace(7.0,0.0,rows) # high velocity at the top (0,0 is top right)
     v = np.zeros_like(ym.T) + np.random.rand(cols,1)-.5
 
     u = u[:,:,np.newaxis]
@@ -144,7 +144,7 @@ def load_directory(path,basename='*',ext='.vec'):
     xarray Dataset with variables and units added as attributes
 
     Input: 
-        directory : path to the directory with .vec, .txt or .VC7 files
+        directory : path to the directory with .vec, .txt or .VC7 files, . can be dropped
 
     Output:
         data : xarray DataSet with dimensions: x,y,t and 
@@ -157,7 +157,7 @@ def load_directory(path,basename='*',ext='.vec'):
     files  = sorted(glob(os.path.join(path,basename+ext)))
     data = []
 
-    if ext == '.vec':
+    if ext.lower().endswith('vec'):
         variables, units, rows, cols, dt, frame = parse_header(files[0])
 
         for i,f in enumerate(files):
@@ -169,13 +169,13 @@ def load_directory(path,basename='*',ext='.vec'):
             combined.attrs['units'] = data[0].attrs['units']
             combined.attrs['dt'] = data[0].attrs['dt']
             combined.attrs['files'] = files
-    elif ext.lower() == '.vc7':
+    elif ext.lower().endswith('vc7'):
         frame = 1
         for i,f in enumerate(files):
-            if basename=='B*':
-                time=int(f[-9:-4])-1
+            if basename == 'B*': # quite strange to have a specific name? 
+                time = int(f[-9:-4])-1
             else:
-                time=i
+                time = i
             data.append(load_vc7(f,time))
         if len(data) > 0:
             combined = xr.concat(data, dim='t')
@@ -291,18 +291,18 @@ def load_vc7(path,time=0):
     #you need to add clear to prevent data leaks
     buff, vatts   =  ReadIM.extra.get_Buffer_andAttributeList(path)
     v_array, buff1 = ReadIM.extra.buffer_as_array(buff)
-    nx=buff.nx
-    nz=buff.nz
-    ny=buff.ny
+    nx = buff.nx
+    nz = buff.nz
+    ny = buff.ny
     #set data range:
     baseRangeX = np.arange(nx)
     baseRangeY = np.arange(ny)
     # baseRangeZ = np.arange(nz)
-    lhs1 =(baseRangeX+0.5)*buff.vectorGrid*buff.scaleX.factor+buff.scaleX.offset  # x-range
-    lhs2 =(baseRangeY+0.5)*buff.vectorGrid*buff.scaleY.factor+buff.scaleY.offset #y-range
-    lhs3 =0
-    lhs4 =0
-    mask =0
+    lhs1 = (baseRangeX+0.5)*buff.vectorGrid*buff.scaleX.factor+buff.scaleX.offset  # x-range
+    lhs2 = (baseRangeY+0.5)*buff.vectorGrid*buff.scaleY.factor+buff.scaleY.offset #y-range
+    lhs3 = 0
+    lhs4 = 0
+    mask = 0
     if buff.image_sub_type<=0: #grayvalue image format
         [lhs1,lhs2] = np.meshgrid(lhs1,lhs2)
         lhs3 =v_array[0,:,:]
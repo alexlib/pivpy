35426a2e5eae14d0df86ebc63c71fa85bf5c2227
Alex Liberzon <alexlib@users.noreply.github.com>
2022-09-26 17:21:20 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,10 +33,18 @@ release = "0.0.16"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [  "sphinx.ext.autodoc", "sphinx.ext.mathjax",
-                "nbsphinx", "sphinx.ext.napoleon",
-                "sphinx.ext.viewcode","myst_parser"
-            ]
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "nbsphinx", 
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "myst_parser"
+    ]
 
 
 # Add any paths that contain templates here, relative to this directory.

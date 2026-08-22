8ef4e41cdf44d95a8fd33f485d70a15dff691ed5
Alex Liberzon <alexlib@users.noreply.github.com>
2026-08-22 21:08:54 +0300

    release: v0.1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ try:
 
     release = _pkg_version("pivpy")
 except Exception:
-    release = "0.1.1"
+    release = "0.1.2"
 
 
 # -- General configuration ---------------------------------------------------
--- pivpy/__init__.py
+++ pivpy/__init__.py
@@ -10,7 +10,7 @@ xr.set_options(keep_attrs=True, display_expand_attrs=False)
 try:
 	__version__ = version("pivpy")
 except PackageNotFoundError:
-	__version__ = "0.1.1"
+	__version__ = "0.1.2"
 
 from .update import check_update, UpdateCheckResult  # noqa: E402,F401
 
